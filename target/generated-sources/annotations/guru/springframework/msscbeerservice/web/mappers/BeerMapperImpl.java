809ff5b733a75c9d0711c5231593299094486412
Zun <zunsamuel97@gmail.com>
2020-09-06 11:02:44 -0500

    step 148

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-05T22:17:00-0500",
+    date = "2020-09-06T11:01:21-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.3 (Oracle Corporation)"
 )
 @Component
